d188e847845b1a4a4b7274e13ab24bd1a18ffb2b
AB <none@mail.org>
2021-04-01 17:54:30 +0000

    Prepare:code structuring.

--- main.c
+++ main.c
@@ -14,58 +14,48 @@
 static const char *vert_shader_src_fp_ = "shaders/v-shader.vert";
 static const char *frag_shader_src_fp_ = "shaders/f-shader.frag";
 
-static struct {
+struct move_uniform {
   int offset_location;
   float x_offset;
   float y_offset;
   float z_offset;
-} current_offsets;
+};
 
 
 unsigned int
 bind_new_buffer(const void *data, int data_size);
 
 
-GLFWwindow *
-glfw_prepare_window(const char *title, int w, int h);
-
-
 void
 key_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
 
 
+unsigned int
+main_prepare_shaders();
+
+
+GLFWwindow *
+main_prepare_window(const char *title, int w, int h);
+
+
 int
 main(void) {
-
-  long vert_src_size = file_size(vert_shader_src_fp_);
-  long frag_src_size = file_size(frag_shader_src_fp_);
-
-  if (check_size_error(vert_src_size, vert_shader_src_fp_) != 0 ||
-      check_size_error(frag_src_size, frag_shader_src_fp_) != 0) {
-
-    exit(EXIT_FAILURE);
-
-  }
-
-  char *v_src = (char *)alloca(vert_src_size + frag_src_size + 2);
-  char *f_src = v_src + vert_src_size + 1;
-
-  v_src[vert_src_size] = '\0';
-  f_src[frag_src_size] = '\0';
-
-  read_file(vert_shader_src_fp_, v_src, vert_src_size);
-  read_file(frag_shader_src_fp_, f_src, frag_src_size);
 
   if (glfwInit() != GLFW_TRUE) {
     fprintf(stderr, "GLFW initialization error\n");
     exit(EXIT_FAILURE);
   }
 
-  GLFWwindow *main_window = glfw_prepare_window("GL", 400, 400);
+  GLFWwindow *main_window = main_prepare_window("GL", 400, 400);
   if (main_window == NULL) {
     glfwTerminate();
     exit(EXIT_FAILURE);
   }
+
+  static struct move_uniform current_offsets;
+
+  glfwSetWindowUserPointer(main_window, &current_offsets);
+
   glfwSetKeyCallback(main_window, key_callback);
 
   printf("OpenGL version string: %s\n", glGetString(GL_VERSION));
@@ -83,7 +73,7 @@ main(void) {
 
   glewInit();
 
-  /*unsigned int buffer_id = */bind_new_buffer(positions, sizeof(positions));
+  bind_new_buffer(positions, sizeof(positions));
 
   glVertexAttribPointer(0,                 /* Attribute index.                */
                         3,                 /* Number of components per vertex */
@@ -92,19 +82,12 @@ main(void) {
                         sizeof(float) * 3, /* Stride delta between 2 elems.   */
                         (const void *)0);  /* Index of attribute inside vert. */
 
-  struct shader_desc targets[] = {
-    { GL_VERTEX_SHADER, v_src, "Vertex" },
-    { GL_FRAGMENT_SHADER, f_src, "Fragment" }
-  };
-
-  unsigned int program = compile_shaders(targets, 2);
-  glUseProgram(program);
   current_offsets.x_offset = 0.0f;
   current_offsets.y_offset = 0.0f;
   current_offsets.z_offset = 0.0f;
-  int loc = glGetUniformLocation(program, "pos_offset");
-  glUniform4f(loc, 0.0f, 0.0f, 0.0f, 0.0f);
-  current_offsets.offset_location = loc;
+
+  current_offsets.offset_location =
+      glGetUniformLocation(main_prepare_shaders(), "pos_offset");
 
   while (!glfwWindowShouldClose(main_window)) {
     glClear(GL_COLOR_BUFFER_BIT);
@@ -120,7 +103,7 @@ main(void) {
 
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
-    glUniform4f(loc, 0.0f, 0.0f, 0.0f, 0.0f);
+    glUniform4f(current_offsets.offset_location, 0.0f, 0.0f, 0.0f, 0.0f);
     glDrawArrays(GL_LINE_LOOP, 3, 4);
 
     glDisableVertexAttribArray(0);
@@ -146,7 +129,7 @@ bind_new_buffer(const void *data, int data_size) {
 
 
 GLFWwindow *
-glfw_prepare_window(const char *title, int w, int h) {
+main_prepare_window(const char *title, int w, int h) {
   GLFWwindow *window = glfwCreateWindow(w, h, title, NULL, NULL);
 
   if (window == NULL) {
@@ -164,6 +147,14 @@ glfw_prepare_window(const char *title, int w, int h) {
 void
 key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
   if (action == GLFW_PRESS) {
+
+    struct move_uniform *mu =
+        (struct move_uniform *)glfwGetWindowUserPointer(window);
+
+    if (mu == NULL) {
+      return;
+    }
+
     float *moveptr = NULL;
     float value = 0.0f;
 
@@ -172,31 +163,62 @@ key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
       glfwSetWindowShouldClose(window, GLFW_TRUE);
 
     } else if (key == GLFW_KEY_LEFT) {
-      moveptr = &current_offsets.x_offset;
+      moveptr = &mu->x_offset;
       value = -0.1f;
     } else if (key == GLFW_KEY_RIGHT) {
-      moveptr = &current_offsets.x_offset;
+      moveptr = &mu->x_offset;
       value = 0.1f;
     } else if (key == GLFW_KEY_DOWN) {
-      moveptr = &current_offsets.y_offset;
+      moveptr = &mu->y_offset;
       value = 0.1f;
     } else if (key == GLFW_KEY_UP) {
-      moveptr = &current_offsets.y_offset;
+      moveptr = &mu->y_offset;
       value = -0.1f;
     } else if (key == GLFW_KEY_Z) {
-      moveptr = &current_offsets.z_offset;
+      moveptr = &mu->z_offset;
       value = 0.1f;
     } else if (key == GLFW_KEY_X) {
-      moveptr = &current_offsets.z_offset;
+      moveptr = &mu->z_offset;
       value = -0.1f;
     }
 
-    if (moveptr) {
+    if (moveptr) *moveptr += value;
 
-      *moveptr += value;
-
-    }
   }
   UNUSED(scancode)
   UNUSED(mods)
+}
+
+
+unsigned int
+main_prepare_shaders() {
+
+  long vert_src_size = file_size(vert_shader_src_fp_);
+  long frag_src_size = file_size(frag_shader_src_fp_);
+
+  if (check_size_error(vert_src_size, vert_shader_src_fp_) != 0 ||
+      check_size_error(frag_src_size, frag_shader_src_fp_) != 0) {
+
+    exit(EXIT_FAILURE);
+
+  }
+
+  char *v_src = (char *)alloca(vert_src_size + frag_src_size + 2);
+  char *f_src = v_src + vert_src_size + 1;
+
+  v_src[vert_src_size] = '\0';
+  f_src[frag_src_size] = '\0';
+
+  read_file(vert_shader_src_fp_, v_src, vert_src_size);
+  read_file(frag_shader_src_fp_, f_src, frag_src_size);
+
+  struct shader_desc targets[] = {
+    { GL_VERTEX_SHADER, v_src, "Vertex" },
+    { GL_FRAGMENT_SHADER, f_src, "Fragment" }
+  };
+
+  unsigned int program = compile_shaders(targets, 2);
+  glUseProgram(program);
+
+  return program;
 }
